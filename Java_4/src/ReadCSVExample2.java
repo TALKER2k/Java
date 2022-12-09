1b8b82ffdd9204008e7a8f02b3c69da1a3d24e7b
TALKER2k <nunikasnikun26504228@yandex.ru>
2022-12-09 21:33:08 +0300

    Java_4

---
+++ Java_4/src/ReadCSVExample2.java
@@ -0,0 +1,60 @@
+import com.opencsv.*;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class People {
+        int id;
+        String name;
+        String gender;
+        String date;
+        Subdivision subdivision = new Subdivision(1);
+        int salary;
+
+        @Override
+        public String toString() {
+                return "People [id=" + this.id + ", Name=" + this.name + ", Gender=" + this.gender + ", Date=" + this.date +", Devision [Id=" + this.subdivision.id +", Name=" + this.subdivision.name+"]"+", Salary=" + this.salary + "]";
+        }
+}
+
+class Subdivision{
+        int id;
+        String name;
+
+        public Subdivision(int id) {
+                this.id=id;
+        }
+        @Override
+        public String toString() {
+                return "Devision [Id=" + this.id +", Name=" + this.name+"]";
+        }
+}
+
+
+public class ReadCSVExample2 {
+        public static void main(String[] args) {
+                People people = new People();
+                ArrayList<People> people2 = new ArrayList<People>();
+                String line = "";
+                String splitBy = ";";
+                try {
+                        BufferedReader br = new BufferedReader(new FileReader("D:/java/forjava.csv"));
+                        while ((line = br.readLine()) != null) {
+                                String[] employee = line.split(splitBy);
+                                people.id = Integer.parseInt(employee[0]);
+                                people.date = employee[3];
+                                people.gender = employee[2];
+                                people.name = employee[1];
+                                people.salary = Integer.parseInt(employee[5]);
+                                Subdivision subs = new Subdivision((int) (Math.random() * 10000));
+                                subs.name=employee[4];
+                                people.subdivision.name=subs.name;
+                                people.subdivision.id=subs.id;
+                                people2.add(people);
+                        }
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+                System.out.println(people2);
+        }
+}
